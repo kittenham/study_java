086d59234755b40974d67c585d156768d36edc48
ITWILL <ITWILL@DESKTOP-2C8UI40>
2023-03-29 12:32:03 +0900

    java

---
+++ java_1/src/basic/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.print("Java");
+		System.out.println("_Programing");
+		System.out.println("Good Luck!!!");
+	}
+}
+
+// 패키지가 없어서 에러뜨는것. 
---
+++ java_1/src/module-info.java
@@ -0,0 +1,4 @@
+module java_1 {
+	//Java 프로젝트의 모듈에서 사용하기 위한 JRE System Library의 모듈을 요구하기 위한 명령
+	requires java.se;        //java_1 모듈에서 java.se를 요구함. 클래스無. 모듈 간의 관계.
+}
