d61a918f326600e313c8369f19f6fb43ebbb5f57
ITWILL <ITWILL@DESKTOP-2C8UI40>
2023-03-30 18:21:10 +0900

    2day

--- java_1/src/example/VariableExample.java
+++ java_1/src/example/VariableExample.java
@@ -8,15 +8,29 @@ public class VariableExample {
 		System.out.println("======================================================");
 		//높이가 9이고 밑변의 길이가 7인 삼각형의 넓이를 계산하여 출력하세요.
 
+		int shv=9, alx=7;
+		System.out.println("삼각형의 넓이 = "+(shv*alx));
 
 		System.out.println("======================================================");
 		//10명의 전체 몸무게가 759Kg인 경우 평균 몸무게를 계산하여 출력하세요.
+		
+		int aha=759, aud=10;
+		System.out.println("평균 몸무게 ="+(aha/aud));
 
 
 		System.out.println("======================================================");
 		//이름이 [홍길동]인 학생이 국어점수 89점, 영어점수 93점, 수학점수 95점을 받은 경우
 		//총점과 평균을 계산하여 이름, 총점, 평균을 출력하세요.
 		//단, 평균은 소숫점 한자리까지만 출력하고 나머지는 절삭 처리 하세요.
+		
+		String name = "홍길동";
+		int kor=89, eng=93, math=95;
+		int tot=(kor+eng+math);
+		double ave=tot/3.0;
+		System.out.println("이름 = "+name+", 총점 = "+tot+", 평균 = "+ave);
+		
+		System.out.println("number(내림) = "+(int)(number*100)/100.0);
+
 
 
 		System.out.println("======================================================");
