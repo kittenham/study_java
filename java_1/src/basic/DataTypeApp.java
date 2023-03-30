1d3de2d8a0b8598c23c8220deca0622ec381c169
ITWILL <ITWILL@DESKTOP-2C8UI40>
2023-03-30 12:32:24 +0900

    2day

--- java_1/src/basic/DataTypeApp.java
+++ java_1/src/basic/DataTypeApp.java
@@ -7,7 +7,7 @@ public class DataTypeApp {
 		System.out.println("<<정수형(IntegerType)>>");
 		System.out.println("정수형(10진수 100) = "+100);	//10진수 정수형 리터럴 출력
 		System.out.println("정수형(8진수 100) = "+0100);	//10진수 앞에 0 붙이면 => 8진수 저수형 리터럴 출력
-		//println() 메소드는 정수값을 무조건 10진수로 변환하여 출력
+			//println() 메소드는 정수값을 무조건 10진수로 변환하여 출력
 		System.out.println("정수형(16진수 100) = "+0x100);	//10진수 앞에 0x 붙이면 => 16진수 저수형 리터럴 출력
 		
 		System.out.println("정수형(10진수 100) = "+100L);	//숫자값L - 정수형 리터럴(8Byte - LongType)
@@ -23,8 +23,8 @@ public class DataTypeApp {
 		//정수값을 표현하기 위한 자료형(키워드) : byte(1Byte), short(2Byte), int(4Byte), long(8Byte)
 		byte a1 = 127;		//1Byte : -128~127
 		short a2 = 32767;		//2Byte : -
-		int a3 = 2147483647;
-		long a4 = 2147483648L;
+		int a3 = 2147483647;		//21억 안 넘으면 int
+		long a4 = 2147483648L;		//21억 넘으면 long
 		
 		System.out.println("a1 = "+a1);
 		System.out.println("a2 = "+a2);
@@ -32,15 +32,16 @@ public class DataTypeApp {
 		System.out.println("a4 = "+a4);
 		
 		//int a5 = 100L;		//값에 대한 손실이 발생할 수 있으므로(가능성임) 에러발생.
-		long a6 = 100;		//숫자에 비해 그릇이 크므로 에러발생X(공간이 남아도는 것이라 효율은 떨어짐. 가독성떨어짐)
+		long a6 = 100;		//숫자에 비해 그릇이 크므로 에러발생X(공간이 남아도는 것이라 효율은 떨어짐(공간낭비). 가독성떨어짐)
 		System.out.println("a6= "+a6);
-		System.out.println("==============================");
+		System.out.println("===========================================");
+		
+		
+		
 		
 		//실수형 리터럴 : 소수점이 있는 숫자값
 		// => Java에서 실수형 리터럴은 기본적으로 8Byte(Double)로 표현 >> 부동 소수점 형식으로 숫자값 표현
 		
-		
-
 		System.out.println("<<실수형(DoubleType)>>");
 		System.out.println("실수값(4Byte) = "+12.3F);	//실수값F >> 4Byte 실수형 리터럴(FloatType)
 		System.out.println("실수값(8Byte) = "+12.3);	//실수값 >> 8Byte 실수형 리터럴(DoubleType)
@@ -50,12 +51,87 @@ public class DataTypeApp {
 		System.out.println("실수값(8Byte) = "+1.23E+10);	//1.23*10의10승
 		
 		//실수값을 표현하기 위한 자료형 - float(4Byte), double(8Byte)
-		float b1=1.23456789F;	//가수부를 표현하기 위한 크기(정밀도) :7자리 (넘으면 반올림됨)
+		float b1=1.23456789F;	//가수부를 표현하기 위한 크기(정밀도) :7자리 (넘으면 반올림됨) //float형 리터럴에 double형 숫자 적으면 error
 		double b2=1.23456789;	//가수부를 표현하기 위한 크기(정밀도) :15자리
 		
 		System.out.println("b1="+b1);
 		System.out.println("b2="+b2);
-		System.out.println("==============================");
+		System.out.println("===========================================");
+		
+		
+		
+		
+		
+		//문자형(CharacterType) 리터럴 : 문자 하나를 ' ' 안에 표현 (문자 여러개X. 무조건 하나)
+		// => Java에서 문자형 리터럴은 기본적으로 2Byte(Character)로 표현 
+		
+		//문자형 리터럴은 일반문자 외에 회피문자(제어문자)가 존재
+		// 회피문자(Escape Character) : 프로그램에서 표현불가능한 문자를 표현하기 위한 문자 - \ 기호 사용
+		// => '\n' : Enter (강제개행), '\t' : Tab, '\'' : '문자, '\"' : "문자, '\\' : \문자, '\0' : NULL문자
+		
+		// 문자형 리터럴은 내부적으로 약속된 정수값(0~65535)으로 표현 : 유니코드Unicode
+		// => Alt키와 함께 문자를 누르면 나오는 값 >> 'A' : 65,  'a' : '97',  '0' : 48,  ' ' : 32,  '\n' : 13,  '가'(완성형) : 44032 
+		
+		System.out.println("<<문자형(CharacterType)>>");
+		System.out.println("문자값(2Byte) = "+'A');
+		System.out.println("문자값(2Byte) = "+'가');
+		System.out.println("문자값(2Byte) = "+'\'');
+		System.out.println("문자값(2Byte) = "+'\\');
+		
+		//문자값을 표현하기 위한 자료형 : Char (2Byte)
+		char c1 = 'A';		// A라는 문자를 c1에 대입
+				//문자형 변수에는 문자값에 대한 약속된 정수값이 저장
+		char c2 = 65;		//문자형 변수에는 문자값 대신 약속된 정수값 저장 가능
+		char c3 = 'a'-32;	//문자값에 대한 연산 결과값 저장 가능
+		
+		System.out.println("c1 = "+c1);
+		System.out.println("c2 = "+c2);
+		System.out.println("c3 = "+c3);
+		
+		char c4 = 45003;
+		System.out.println("c4 = "+c4);
+		System.out.println("===========================================");
+
+		
+		
+		
+		//논리형(BooleanType) 리터럴 : false(거짓), true(참) => 연산 불가능
+		// => Java에서 문자형 리터럴은 기본적으로 1Byte(BooleanType)로 표현
+		
+		System.out.println("<<논리형(BooleanType)>>");
+		System.out.println("논리값(1byte) = "+false);
+		System.out.println("논리값(1byte) = "+true);
+		//관계 연산자(비교 연산자)를 이용한 연산식의 결과값으로 논리형 리터럴을 사용(제공)
+		
+		System.out.println("논리값(1byte) = "+(20>10));
+		System.out.println("논리값(1byte) = "+(20<10));
+
+		//논리값을 표현하기 위한 자료형 : boolean(2byte)
+		boolean d1 = false;
+		boolean d2 = true;
+		
+		System.out.println("d1 = "+d1);
+		System.out.println("d2 = "+d2);
+		System.out.println("===========================================");
+
+		
+		
+		
+		
+		//문자열(StringType) 리터럴 : " " 안에 0개 이상의 문자들로 표현
+		//=> 문자열은 원시형(기본형)이 아닌 참조형으로 String 클래스로 표현
+		
+		System.out.println("<<문자형(StringType)>>");
+		System.out.println("문자열 = "+"홍길동");
+		System.out.println("문자열 = "+"유관순 열사가 대한독립만세를 외쳤습니다."); 
+			//대한독립만세를 강조할 때 ""바로 쓰면 X(error). \" 사용!
+		System.out.println("문자열 = "+"유관순 열사가 \"대한독립만세\"를 외쳤습니다."); 
+		
+		//문자열을 표현하기 위한 자료형 : String 클래스 = String 객체
+		String name = "임꺽정";
+		System.out.println("이름 = "+name);
+		System.out.println("===========================================");
+
 		
 		
 	}
--- java_1/src/basic/VariableApp.java
+++ java_1/src/basic/VariableApp.java
@@ -1,7 +1,7 @@
 package basic;
 
-//리터럴(Literal)
-//변수(Variable) : 리터럴(값)을 저장하기 위해 메모리memory에 부여된 이름
+//리터럴(Literal) : 프로그램에서 사용하기 위해 표현되는 값(value)
+//변수(Variable) : 리터럴(값)을 저장하기 위해 메모리memory에 부여된 이름[식별자]
 
 public class VariableApp {
 	public static void main(String[] args) {
@@ -13,18 +13,18 @@ public class VariableApp {
 		//int su://동일한 이름의 변수를 재선언 할 경우 에러 발생
 		su = 200; 		//기존에 변수 su에 저장되어 있던 100은 삭제됨. 새로운 값200 저장.
 		System.out.print("변경값 = ");
-		System.out.println(su);
+		System.out.println(su); 	// 변수에 저장된 값을 화면에 출력
 		
 		System.out.println("==============================");
 		int num = 100;		//변수 선언 및 초기값 저장
-		System.out.println("num = " + num);
+		System.out.println("num = " +num);		//여기서 "+"는 더하기가 아니라 결합연산자 : 문자열을 결합함
 	
 		// "문자열"+값 또는 값+"문자열" >> 피연산자가 하나라도 문자열이면(문자열과 값 결합) 문자열 -> 결과값 : 문자열
-		System.out.println("num = " + num);
+		System.out.println("num = " +num);
 		System.out.println("==============================");
 		System.out.println("올해는 "+2+0+2+3+"년입니다.");		//올해는 2023년 입니다.
 		System.out.println(2+0+2+3+"년은 토끼띠해입니다.");		
-		//7년은 토끼띠해입니다. =>실행오류.(잘못된 결과 발생) - 앞에 문자열 없이 숫자만 있기 때문에 더해짐.
+		//7년은 토끼띠해입니다. => 실행오류.(잘못된 결과 발생. 실행은 되지만 결과가 이상하게 나오는 것) - 앞에 문자열 없이 숫자만 있기 때문에 더해짐.
 		//프로그램을 읽을 때 왼쪽부터 오른쪽으로 읽기 때문에 맨 앞이 문자열이면 문자열로 인식한 이후로는 숫자가 와도 전부 문자열로 인식함.
 		//앞에 문자열 없이 숫자가 오면 숫자자체로 인식하기 때문에 연산자가 있으면 연산을 진행. 그러나 그 이후에 문자열이 또 온다면 그 이후로는 숫자가 와도 문자열로 인식한다.
 		System.out.println(""+2+0+2+3+"년은 토끼띠해입니다.");		
@@ -34,7 +34,7 @@ public class VariableApp {
 
 		//동일한 자료형의 변수는, 기호를 사용하여 나열선언 가능.
 		int num1 = 100, num2 = 200;
-		System.out.println("연산결과 = "+num1+num2); // 잘못된 결과 발생. 더하기가 안됨. 문자열로 인식.
+		System.out.println("연산결과 = "+num1+num2); // 잘못된 결과 발생. 더하기가 안됨. 문자열로 인식.(왼쪽부터 읽으니까)
 		System.out.println("연산결과 = "+(num1+num2)); // 디버깅. 원하던 결과. 300. 계산 우선순서 : () → *,/ → +,-
 		System.out.println("연산결과 = "+(num1*num2));
 		System.out.println("연산결과 = "+num1*num2);
@@ -45,7 +45,7 @@ public class VariableApp {
 		int kor = 88, eng = 90;
 		int tot = kor + eng;		//연산 결과를 변수(tot)에 저장. 국어점수+영어점수(88+90이지만 점수가 바뀌면 변수에 들어가는 값도 바뀔 수 있도록)
 		// 값은 무조건 변수에다가 저장해서 사용하기~
-		System.out.println("점수 합계 = " + tot);
+		System.out.println("점수 합계 = " +tot);
 		
 	
 		System.out.println("==============================");
