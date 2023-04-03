07fecbb7c5ea14bc4148ae235c4348cf28e560f8
ITWILL <ITWILL@DESKTOP-2C8UI40>
2023-04-03 15:46:01 +0900

    4day

--- java_1/src/basic/MultiForApp.java
+++ java_1/src/basic/MultiForApp.java
@@ -17,13 +17,22 @@ public class MultiForApp {
 		//★ 출력하기
 		System.out.println("★");
 		
+		System.out.println("===========================================");
+
+		
 		//★★★★★★★ 출력하기
 		System.out.println("★★★★★★★"); //별을 더 많이 출력하려면 효율적인 방법X
+		
+		System.out.println("===========================================");
+
 		
 		for(int i=1;i<=7;i++) {
 			System.out.print("★");
 		}
 		System.out.println();
+		
+		System.out.println("===========================================");
+
 		
 		//★★★★★★★ 4줄 출력하기
 		for(int j=1;j<=4;j++) {		//"행(Row)" 처리 위한 반복문
@@ -81,6 +90,97 @@ public class MultiForApp {
 		
 		System.out.println("===========================================");
 		
+		//★★★★★
+		//★★★★★
+		//★★★★★
+		//★★★★★
+		//★★★★★
+		//중첩반복문 사용시 디버깅 하면 반복순서 볼수있음~
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				System.out.print("★");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("===========================================");
+
+		
+		//★			i=1, 별=1
+		//★★			i=2, 별=2
+		//★★★		i=3, 별=3
+		//★★★★		i=4, 별=4
+		//★★★★★	i=5, 별=5
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("★");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("===========================================");
+		//★★★★★
+		//★★★★
+		//★★★
+		//★★
+		//★
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=(6-i);j++) {
+				System.out.print("★");
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+
+		for(int i=5;i>=1;i--) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("★");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("===========================================");
+		//☆☆☆☆★	i=1, 흰별=4, 검은별=1
+		//☆☆☆★★	i=2, 흰별=3, 검은별=2
+		//☆☆★★★	i=3, 흰별=2, 검은별=3
+		//☆★★★★	i=4, 흰별=1, 검은별=4
+		//★★★★★	i=5, 흰별=0, 검은별=5
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=4;j>=i;j--) {		//for(int j=4;j<=(5-i);i--);
+				System.out.print("☆");
+				
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print("★");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("===========================================");
+		//1 2 3 4 5  >>j
+		//☆☆☆☆★	i=1, 흰별=4, 검은별=1
+		//☆☆☆★★	i=2, 흰별=3, 검은별=2
+		//☆☆★★★	i=3, 흰별=2, 검은별=3
+		//☆★★★★	i=4, 흰별=1, 검은별=4
+		//★★★★★	i=5, 흰별=0, 검은별=5
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {		//for(int j=4;j<=(5-i);i--);
+				if(i+j<6) {			//검은별과 흰별의 경계에 있는 흰 별의 위치를 보고 (i,j) 행렬따져서 계산
+					System.out.print("☆");
+				} else {
+				System.out.print("★");
+				}	
+			}
+			System.out.println();
+		}
+		
+		System.out.println("===========================================");
 		
 	}
 }
